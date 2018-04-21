ff016c6c5c251c010b7f1ed979ff53b5fb77b0a6
Gunter Rotsaert <gunter@mydeveloperplanet.com>
2018-04-21 14:13:39 +0200

    Hello Kubernetes Spring Boot app

---
+++ src/main/java/com/mydeveloperplanet/mykubernetesplanet/MyKubernetesPlanetApplication.java
@@ -0,0 +1,12 @@
+package com.mydeveloperplanet.mykubernetesplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyKubernetesPlanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyKubernetesPlanetApplication.class, args);
+	}
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module com.mydeveloperplanet.mykubernetesplanet {
+    requires spring.web;
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
+}
