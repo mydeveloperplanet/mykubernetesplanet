711bed2df55914ea0ba838700039a4471eda1723
Gunter Rotsaert <gunter@mydeveloperplanet.com>
2018-04-29 12:04:34 +0200

    Updated version to 0.0.2 and added host address to hello endpoint

--- src/main/java/com/mydeveloperplanet/mykubernetesplanet/HelloController.java
+++ src/main/java/com/mydeveloperplanet/mykubernetesplanet/HelloController.java
@@ -3,12 +3,22 @@ package com.mydeveloperplanet.mykubernetesplanet;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class HelloController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello Kubernetes!";
+        StringBuilder message = new StringBuilder("Hello Kubernetes!");
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            message.append(" From host: " + ip);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return message.toString();
     }
 
 }
